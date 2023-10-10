b69330f980a8c063d0f3f4b2a2ac794c5c454d6f
Suroj Maharjan <surojcodes@gmail.com>
2023-10-10 16:24:41 -0500

    Company input validation

--- src/routes/company.routes.ts
+++ src/routes/company.routes.ts
@@ -5,12 +5,20 @@ import {
 } from '../controllers/company.controller';
 import requireUser from '../middleware/requireUser';
 import authorizeUser from '../middleware/authorizeUser';
+import { companySchema } from '../schema/company.schema';
+import validate from '../middleware/validateResource';
+import asyncWrapper from '../utils/asyncWrapper';
 
 const router = express.Router();
 
 router
   .route('/')
-  .get(requireUser, authorizeUser(['admin']), createCompany)
-  .put(requireUser, authorizeUser(['admin']), updateCompany);
+  .get(requireUser, authorizeUser(['admin']), asyncWrapper(createCompany))
+  .put(
+    validate(companySchema),
+    requireUser,
+    authorizeUser(['admin']),
+    asyncWrapper(updateCompany)
+  );
 
 export default router;
---
+++ src/schema/company.schema.ts
@@ -0,0 +1,22 @@
+import z from 'zod';
+
+export const companySchema = z.object({
+  body: z.object({
+    name: z.string({ required_error: 'Company name is required' }),
+    address: z.array(
+      z.object({
+        addr_line1: z.string({ required_error: 'Addess Line 1 is required' }),
+        addr_line2: z.string({ required_error: 'Addess Line 2 is required' }),
+        city: z.string({ required_error: 'City is required' }),
+        zip: z.string({ required_error: 'Zip is required' }),
+        state: z.string({ required_error: 'State is required' }),
+        country: z.string({ required_error: 'Country is required' }),
+      })
+    ),
+    contact: z.array(z.string()),
+    email: z
+      .string({ required_error: 'Email is required' })
+      .email('Invalid email'),
+    logo: z.string(),
+  }),
+});
